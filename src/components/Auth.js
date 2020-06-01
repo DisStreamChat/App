1b87e7889b95f63cd563c0b3cee1bb7f642cb666
David Snyder <davidgraygs4@gmail.com>
2020-06-01 09:44:48 -0400

    updated oauth url

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -134,7 +134,7 @@ const Auth = props => {
         });
 
         // open a popup window to the twitch oauth url
-        window.open(`https://id.twitch.tv/oauth2/authorize?client_id=ip3igc72c6wu7j00nqghb24duusmbr&redirect_uri=https://distwitchchat-backend.herokuapp.com/oauth/twitch/&response_type=code&scope=openid%20moderation:read`)
+        window.open(`https://id.twitch.tv/oauth2/authorize?client_id=ip3igc72c6wu7j00nqghb24duusmbr&redirect_uri=https://api.distwitchchat.com/oauth/twitch/&response_type=code&scope=openid%20moderation:read`)
     }, [props.history])
 
     return firebase.auth.currentUser ? <Redirect to="/" /> : (
