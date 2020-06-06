731866932ae85655fa88482569a6696fc07e5e76
David Snyder <davidgraygs4@gmail.com>
2020-06-05 21:46:47 -0400

    added settings v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import "./Header.css"
 import firebase from "../firebase"
 import { withRouter, Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import Setting from "./Setting"
 
+const defaults = {
+    TwitchColor: "#462b45",
+    YoutubeColor: "#c4302b",
+    discordColor: "#2d688d",
+    highlightedMessageColor: "#6e022e"
+}
 
 const Header = props => {
     const [settingsOpen, setSettingsOpen] = useState(false)
+    const [appSettings, setAppSettings] = useState()
+    const currentUser = firebase.auth.currentUser
+    const id = currentUser.uid
 
     const clearChat = useCallback(() => {
         props.setMessages(m => m.map(msg => {return {...msg, deleted: true}}))
@@ -21,12 +31,33 @@ const Header = props => {
         props.setMessages(messages => messages.map(msg => { return { ...msg, deleted: msg.deleted || msg.platform === "discord"}}))
     }, [props])
 
+    const updateAppSetting = useCallback(async (name, value) => {
+        const copy = { ...appSettings }
+        copy[name] = value
+        const userRef = firebase.db.collection("Streamers").doc(id)
+        await userRef.update({
+            appSettings: copy
+        })
+    }, [appSettings, id])
+
     const signout = useCallback(async () => {
         await firebase.logout()
         localStorage.removeItem("userId")
         localStorage.removeItem("messages")
         props.history.push("/")
     }, [props.history]) 
+
+    useEffect(() => {
+        (async () => {
+            const unsub = firebase.db.collection("Streamers").doc(id).onSnapshot(snapshot => {
+                const data = snapshot.data()
+                if (data) {
+                    setAppSettings(data.appSettings)
+                }
+            })
+            return unsub
+        })()
+    }, [id, props.history])
 
     return (
         <header className={`header ${props.pad} ${settingsOpen && "open"}`} >
@@ -45,7 +76,9 @@ const Header = props => {
                 }
             </nav>
             <div className="header-settings">
-
+                {Object.entries(appSettings || {}).sort().sort((a, b) => typeof a[1] === "boolean" ? -1 : 1).map(([key, value]) => {
+                    return <Setting key={key} default={defaults[key]} onChange={updateAppSetting} name={key} value={value} type={typeof value === "boolean" ? "boolean" : "color"} />
+                })}
             </div>
             <div className="header-lower" onClick={() => setSettingsOpen(o => !o)}>
                 <KeyboardArrowDownIcon className={`chevron ${settingsOpen && "open"}`}/>
---
+++ src/components/Setting.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import { ChromePicker } from "react-color"
+import { useEffect } from 'react';
+import { Switch } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { blueGrey } from '@material-ui/core/colors';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import "./Users.css"
+
+
+const FancySwitch = withStyles({
+    root: {
+        padding: 7,
+    },
+    thumb: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#fff',
+        boxShadow:
+            '0 0 12px 0 rgba(0,0,0,0.08), 0 0 8px 0 rgba(0,0,0,0.12), 0 0 4px 0 rgba(0,0,0,0.38)',
+    },
+    switchBase: {
+        color: 'rgba(0,0,0,0.38)',
+        padding: 7,
+    },
+    track: {
+        borderRadius: 20,
+        backgroundColor: blueGrey[300],
+    },
+    checked: {
+        '& $thumb': {
+            backgroundColor: '#fff',
+        },
+        '& + $track': {
+            opacity: '1 !important',
+        },
+    },
+    focusVisible: {},
+})(Switch);
+
+const Setting = props => {
+    const [value, setValue] = useState(props.value)
+    const [open, setOpen] = useState(false)
+    
+    const changeHandler = v => {
+        props.onChange(props.name, v)
+    }
+
+    useEffect(() => {
+        if (props.type === "color") {
+            setValue(props.value || props.default)
+        }else{
+            setValue(props.value)
+        }
+    }, [props])
+
+    return (
+        <div className={`setting ${props.type === "color" && "color-setting"} ${open && "open"}`}>
+            {props.type === "color" ? 
+                <>
+                    <div className="color-header" onClick={() => setOpen(o => !o)}>
+                        <h3>{props.name}</h3>
+                        <div className="color-preview" style={{
+                            background: value || "#000",
+                        }}></div>
+                        <KeyboardArrowDownIcon className={open && "open"}/>
+                    </div>
+                    <ChromePicker
+                        color={value}
+                        onChange={color => changeHandler(color.hex)}
+                        disableAlpha
+                    />
+                    <button onClick={() => changeHandler(props.default)}>reset</button>
+                </>
+                :
+                <span className="checkbox-setting">
+                    <FormControlLabel control={<FancySwitch color="primary" checked={value} onChange={e => changeHandler(e.target.checked)} name={props.name} />} label={props.name}/>
+                </span>
+            }
+        </div>
+    );
+}
+
+export default Setting;
