2271dc1605c9256a514b4b85fa2eaeb592fe6e58
David Snyder <davidgraygs4@gmail.com>
2020-06-29 21:10:22 -0400

    move searchbox to seperate file

---
+++ src/components/SearchBox.js
@@ -0,0 +1,24 @@
+import React, {useState, useCallback} from "react"
+import ClearRoundedIcon from "@material-ui/icons/ClearRounded";
+
+
+const SearchBox = React.memo(({ onChange }) => {
+	const [value, setValue] = useState("");
+
+	const handleChange = useCallback(
+		e => {
+			setValue(e.target.value);
+			onChange(e.target.value);
+		},
+		[onChange]
+	);
+
+	return (
+		<div className="search-container">
+			<input value={value} onChange={handleChange} type="text" name="" id="" placeholder="Search Messages" className="settings--searchbox" />
+			<ClearRoundedIcon className="clear-button" onClick={() => setValue("")} />
+		</div>
+	);
+});
+
+export default SearchBox
