f84457f9a4aa78a70e899c1b352bcac24b14323d
David Snyder <davidgraygs4@gmail.com>
2020-07-04 13:33:38 -0400

    removed old header buttons and added new stats

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import firebase from "../firebase";
 import Button from "@material-ui/core/Button";
 import Setting from "./Setting";
 import SearchBox from "./SearchBox";
+import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone';
 import "./Header.css";
 
 const SettingList = props => {
@@ -44,18 +45,63 @@ const Header = props => {
 	const [appSettings, setAppSettings] = useState();
 	const [defaultSettings, setDefaultSettings] = useState();
 	const [search, setSearch] = useState();
-    const [chatHeader, setChatHeader] = useState(false);
-    const [show, setShow] = useState(true)
+	const [chatHeader, setChatHeader] = useState(false);
+	const [show, setShow] = useState(true);
 	const currentUser = firebase.auth.currentUser;
-    const id = currentUser?.uid || " ";
-    const {setMessages} = useContext(AppContext)
-    
-    
-    const {location} = props
+	const id = currentUser?.uid || " ";
+	const { setMessages } = useContext(AppContext);
+	const [viewingUserId, setViewingUserId] = useState();
+	const [viewingUserInfo, setViewingUserInfo] = useState();
+	const [viewingUserStats, setViewingUserStats] = useState();
+	const { location } = props;
+
+	useEffect(() => {
+        setViewingUserId(location.pathname.split("/").slice(-1)[0]);
+        setViewingUserStats(null)
+	}, [location]);
+
+	useEffect(() => {
+		(async () => {
+			if (chatHeader && show) {
+				const userRef = firebase.db.collection("Streamers").doc(viewingUserId || " ");
+				const userDoc = await userRef.get();
+				const userData = userDoc.data();
+				setViewingUserInfo(userData);
+			}
+		})();
+	}, [chatHeader, show, viewingUserId]);
+
+	useEffect(() => {
+		const id = setInterval(async () => {
+			if (viewingUserInfo) {
+				// const ApiUrl = `${process.env.REACT_APP_SOCKET_URL}/stats/twitch/?name=instafluff`;
+				const ApiUrl = `${process.env.REACT_APP_SOCKET_URL}/stats/twitch/?name=${viewingUserInfo.name}`;
+				const response = await fetch(ApiUrl);
+				const data = await response.json();
+				setViewingUserStats(prev => {
+					if (data) {
+						return {
+							name: viewingUserInfo.displayName,
+							viewers: data.viewer_count,
+							isLive: true,
+						};
+					} else {
+						return {
+							name: viewingUserInfo.displayName,
+							viewers: 0,
+							isLive: false,
+						};
+					}
+                });
+			}
+		}, 10000);
+		return () => clearInterval(id);
+	}, [viewingUserInfo]);
+
 	useEffect(() => {
 		setChatHeader(location?.pathname?.includes("chat"));
-        setShow(!location?.pathname?.includes("login"))
-    }, [location]);
+		setShow(!location?.pathname?.includes("login"));
+	}, [location]);
 
 	useEffect(() => {
 		(async () => {
@@ -78,30 +124,6 @@ const Header = props => {
 		return unsub;
 	}, [id]);
 
-	const clearChat = useCallback(() => {
-		setMessages(m =>
-			m.map(msg => {
-				return { ...msg, deleted: true };
-			})
-		);
-	}, [setMessages]);
-
-	const clearTwitch = useCallback(() => {
-		setMessages(messages =>
-			messages.map(msg => {
-				return { ...msg, deleted: msg.deleted || msg.platform === "twitch" };
-			})
-		);
-	}, [setMessages]);
-
-	const clearDiscord = useCallback(() => {
-		setMessages(messages =>
-			messages.map(msg => {
-				return { ...msg, deleted: msg.deleted || msg.platform === "discord" };
-			})
-		);
-	}, [setMessages]);
-
 	const updateAppSetting = useCallback(
 		async (name, value) => {
 			const copy = { ...appSettings };
@@ -120,21 +142,17 @@ const Header = props => {
 		props.history.push("/");
 	}, [props.history]);
 
-	return !show ? <></> :(
+	return !show ? (
+		<></>
+	) : (
 		<header className={`header ${settingsOpen && "open"}`}>
 			<nav className="nav">
-				{chatHeader && (
-					<>
-						<Button variant="contained" onClick={clearChat}>
-							Clear Chat
-						</Button>
-						<Button variant="contained" onClick={clearTwitch}>
-							Clear Twitch Chat
-						</Button>
-						<Button variant="contained" onClick={clearDiscord}>
-							Clear Discord Chat
-						</Button>
-					</>
+				{chatHeader && viewingUserStats && (
+					<div className="stats">
+						<div className={`live-status ${viewingUserStats?.isLive ? "live" : ""}`}></div>
+						<div className="name">{viewingUserStats?.name}</div>
+						<div className={`live-viewers`}><PeopleAltTwoToneIcon/>{viewingUserStats?.viewers}</div>
+					</div>
 				)}
 				{chatHeader ? (
 					<Link to="/channels">
@@ -149,7 +167,7 @@ const Header = props => {
 				)}
 			</nav>
 			<div className="header-settings">
-				<SearchBox onChange={setSearch} placeHolder="Search Settings"/>
+				<SearchBox onChange={setSearch} placeHolder="Search Settings" />
 				<SettingList all search={search} defaultSettings={defaultSettings} settings={appSettings} updateSettings={updateAppSetting} />
 			</div>
 			<div className="header-lower" onClick={() => setSettingsOpen(o => !o)}>
