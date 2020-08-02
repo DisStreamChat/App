172a04d0652ca54f67d6ef295da3f9f70467f2ad
David Snyder <davidgraygs4@gmail.com>
2020-08-01 21:16:27 -0400

    auto formatting

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,20 +1,9 @@
-import React from 'react';
-import { Route, Redirect } from "react-router-dom"
-import firebase from "../firebase"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import firebase from "../firebase";
 
 const ProtectedRoute = React.memo(({ component: RouteComponent, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            render={routeProps =>
-                !!firebase.auth.currentUser ? (
-                    <RouteComponent {...routeProps} />
-                ) : (
-                        <Redirect to="/login" />
-                    )
-            }
-        />
-    );
-})
+	return <Route {...rest} render={routeProps => (!!firebase.auth.currentUser ? <RouteComponent {...routeProps} /> : <Redirect to="/login" />)} />;
+});
 
 export default ProtectedRoute;
