5c654258553e90dc6211450bb6d4b86ef713af25
David Snyder <davidgraygs4@gmail.com>
2020-08-11 20:04:46 -0400

    added terms requirement to sign in page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,32 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import firebase from "../firebase";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import { withRouter } from "react-router";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import "./Auth.scss";
 const { ipcRenderer } = window.require("electron");
 
 const { remote } = window.require("electron");
+
+const Anchor = ({ children, ...rest }) => {
+	return <a {...rest}>{children}</a>;
+};
+
+const A = props => {
+	const elementProps = {
+		href: props.local ? "" : props.href,
+		to: props.local ? props.href : "",
+		className: props.className,
+		target: props.newTab && "_blank",
+		rel: props.newTab && "noopener noreferrer",
+		disabled: props.disabled,
+	};
+
+	const Element = props.local ? Link : Anchor;
+
+	return <Element {...elementProps}>{props.children}</Element>;
+};
 
 const Auth = React.memo(props => {
 	const signInWithGoogle = useCallback(async () => {
@@ -27,6 +46,7 @@ const Auth = React.memo(props => {
 			console.log(err.message);
 		}
 	}, [props.history]);
+	const [readTerms, setReadTerms] = useState(false);
 
 	const loginWithTwitch = useCallback(async () => {
 		try {
@@ -46,8 +66,8 @@ const Auth = React.memo(props => {
 			async function receiveMessage(event, data) {
 				console.log(data);
 				const json = data;
-                await firebase.auth.signInWithCustomToken(json.token);
-                props.history.push("/")
+				await firebase.auth.signInWithCustomToken(json.token);
+				props.history.push("/");
 			}
 
 			// open a popup window to the twitch oauth url
@@ -65,16 +85,36 @@ const Auth = React.memo(props => {
 			<div className="Modal">
 				<h1 className="modal-heading">Login to DisStreamChat</h1>
 				<h2 className="modal-subheading">Connect with:</h2>
-				<div className="modal-buttons">
-					<button onClick={loginWithTwitch} className="modal-button twitch">
+				<form onSubmit={e => e.preventDefault()} className="modal-buttons">
+					<button type="submit" onClick={readTerms ? loginWithTwitch : () => {}} className="modal-button twitch">
 						<img src={`${process.env.PUBLIC_URL}/social-media.svg`} alt="" width="20" className="logo-icon" />
 						Twitch
 					</button>
-					<div className="modal-button youtube" onClick={signInWithGoogle}>
+					<button className="modal-button youtube" onClick={readTerms ? signInWithGoogle : () => {}}>
 						<YouTubeIcon className="logo-icon yt-icon" />
 						YouTube
+					</button>
+					<div className="legal">
+						<input
+							required
+							value={readTerms}
+							onChange={e => setReadTerms(e.target.checked)}
+							id="terms-check"
+							type="checkbox"
+							name="terms"
+						/>
+						<label htmlFor="terms-check">
+							I accept the{" "}
+							<A href="/terms" local newTab>
+								terms and conditions
+							</A>{" "}
+							and{" "}
+							<A href="/privacy" local newTab>
+								privacy policy
+							</A>
+						</label>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
